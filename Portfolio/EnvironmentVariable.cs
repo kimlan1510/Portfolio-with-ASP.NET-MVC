b52b000571a13dec7c7b9a91b577c75bc9e83c5a
kimlan nguyen <kimlan1510@yahoo.com>
2017-08-25 11:04:46 -0700

    Retrieve access token from github

---
+++ Portfolio/EnvironmentVariable.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Portfolio
+{
+    public class EnvironmentVariable
+    {
+        public string AccessToken = "285bf175037f15e86800182a3e9ab958a9518497" ;
+    }
+}
